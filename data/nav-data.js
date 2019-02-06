758bb24cb934261f62e7b419eb0c1bdfc7293cf2
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-02-06 15:49:32 +0000

    Deploy DostalTomas/angularjs-value-editor to github.com/DostalTomas/angularjs-value-editor.git:gh-pages

--- data/nav-data.js
+++ data/nav-data.js
@@ -8,7 +8,52 @@ angular.module('docApp').constant('DOCS_NAVIGATION', {
         "name": "angularjs-value-editor",
         "type": "groups",
         "href": "api/angularjs-value-editor",
-        "navItems": []
+        "navItems": [
+          {
+            "name": "component",
+            "type": "section",
+            "href": "api/angularjs-value-editor/component",
+            "navItems": [
+              {
+                "name": "kpValueEditor",
+                "type": "component",
+                "href": "api/angularjs-value-editor/component/kpValueEditor"
+              },
+              {
+                "name": "textValueEditor",
+                "type": "component",
+                "href": "api/angularjs-value-editor/component/textValueEditor"
+              }
+            ]
+          },
+          {
+            "name": "type",
+            "type": "section",
+            "href": "api/angularjs-value-editor/type",
+            "navItems": [
+              {
+                "name": "TextValueEditorOptions",
+                "type": "type",
+                "href": "api/angularjs-value-editor/type/TextValueEditorOptions"
+              },
+              {
+                "name": "TextValueEditorValidations",
+                "type": "type",
+                "href": "api/angularjs-value-editor/type/TextValueEditorValidations"
+              },
+              {
+                "name": "ValueEditorOptions",
+                "type": "type",
+                "href": "api/angularjs-value-editor/type/ValueEditorOptions"
+              },
+              {
+                "name": "ValueEditorValidations",
+                "type": "type",
+                "href": "api/angularjs-value-editor/type/ValueEditorValidations"
+              }
+            ]
+          }
+        ]
       }
     ]
   }
---
+++ examples/example-textValueEditorExample/script.js
@@ -0,0 +1,4 @@
+(function(angular) {
+  'use strict';
+angular.module('textValueEditorExample', ['angularjs-value-editor']);
+})(window.angular);
