8e5e23ed4e98344a0e72072786d6c264efaeab93
Tomas Dostal <Tomas.Dostal@kpsys.cz>
2019-03-01 13:35:10 +0100

    Because karma-istanbul plugin has wrong determining of branches passing, branches coverage threshold had to be decreased.

--- demo/app.ts
+++ demo/app.ts
@@ -1,3 +1,10 @@
+declare global {
+    interface Window {
+        jQuery: JQueryStatic;
+        $: JQueryStatic;
+    }
+}
+
 import * as jQuery from 'jquery';
 import 'angular';
 import register from '@kpsys/angularjs-register';
@@ -5,7 +12,7 @@ import './required-vendors';
 import valueEditorModule from '../dist/angularjs-value-editor';
 import '../dist/angularjs-value-editor.css';
 import DemoController from './demo.controller';
-// @ts-ignore
+
 window.$ = window.jQuery = jQuery;
 
 register('app', [valueEditorModule])
