847aee37bb2e3549af674ed781485a098f071e4b
DostalTomas <DostalTomas@users.noreply.github.com>
2020-08-31 16:24:01 +0000

    deploy: 50116ec424b6162ff89d53aee7bd22c8c6ab9ca1

--- examples/example-accessNumberValueEditorExample/script.js
+++
@@ -1,7 +0,0 @@
-(function(angular) {
-  'use strict';
-angular.module('accessNumberValueEditorExample', ['angularjs-value-editor'])
- .config((accessNumberValueEditorConfigurationServiceProvider) => accessNumberValueEditorConfigurationServiceProvider.setConfiguration({
-     dataSource: () => Promise.resolve(['one', 'two', 'three'])
- }));
-})(window.angular);
--- examples/example-signatureValueEditorExample/script.js
+++
@@ -1,7 +0,0 @@
-(function(angular) {
-  'use strict';
-angular.module('signatureValueEditorExample', ['angularjs-value-editor'])
- .config((signatureValueEditorConfigurationServiceProvider) => signatureValueEditorConfigurationServiceProvider.setConfiguration({
-     dataSource: () => Promise.resolve(['one', 'two', 'three'])
- }));
-})(window.angular);
